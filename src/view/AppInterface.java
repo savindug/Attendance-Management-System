3e9cadeb14b0046aca68d3513f831dc0745a2071
savindug <savibashnon11@gmail.com>
2019-12-27 06:25:28 +0530

    Merge origin/master export to excel Conflicts: 	nbproject/project.properties 	src/view/AppInterface.form 	src/view/AppInterface.java

--- src/view/AppInterface.java
+++ src/view/AppInterface.java
@@ -1018,6 +1018,12 @@ public class AppInterface extends javax.swing.JFrame {
             }
         });
 
+        jFileChooser1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jFileChooser1ActionPerformed(evt);
+            }
+        });
+
         btnbackaddreports4.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         btnbackaddreports4.setText("Back");
         btnbackaddreports4.addActionListener(new java.awt.event.ActionListener() {
@@ -1634,7 +1640,7 @@ public class AppInterface extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                        .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, 0, Short.MAX_VALUE)
                         .addContainerGap())))
         );
         layout.setVerticalGroup(
@@ -2053,6 +2059,11 @@ public class AppInterface extends javax.swing.JFrame {
         tableot.setModel(DbUtils.resultSetToTableModel(es.searchOTList(em)));
        
     }//GEN-LAST:event_localotsearchActionPerformed
+
+    private void jFileChooser1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jFileChooser1ActionPerformed
+        // TODO add your handling code here:
+        JFileChooser jf = new JFileChooser();
+    }//GEN-LAST:event_jFileChooser1ActionPerformed
 
     
     
